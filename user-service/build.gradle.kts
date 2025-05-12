a0728d64e6344b03f1edda5aaa08820fce4d1c11
Szymon Roziewski <szymon.roziewski@opi.org.pl>
2025-05-12 12:21:22 +0200

    Switch to YAML config and add PostgreSQL & Flyway support
    Converted application properties to YAML for better structure. Added PostgreSQL configuration and dependencies for database connectivity. Integrated Flyway for database migration management. Updated build script to include necessary plugins and libraries.

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.flywaydb.flyway") version "9.16.0"
 }
 
 group = "com.autohub.user-service"
@@ -32,9 +33,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springframework.session:spring-session-data-redis")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
